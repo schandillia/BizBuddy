54f668dc4ec2b46d46274395c0746fdc61b1426e
schandillia <amit@schandillia.com>
2025-01-01 12:06:15 +0530

    feat(config): improve performance and build settings
    - Enabled SWC minification for faster builds and smaller bundles.
    - Added experimental CSS optimization and scroll restoration.
    - Set custom build output directory to 'build'.
    
    These changes enhance performance and optimize the development workflow.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.mjs
+import withBundleAnalyzer from "@next/bundle-analyzer"
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  swcMinify: true, // Use the SWC compiler for faster builds and smaller bundles
+  experimental: {
+    optimizeCss: true, // Inline critical CSS
+    scrollRestoration: true, // Improves UX for page reloads/navigation
+  },
+  distDir: "build", // Custom build output folder
+
+  // Your other Next.js config options here, if any
+}
+
+const config = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig)
+
+export default config
