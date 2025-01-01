e4e99fc14fcca0f7492604188f5834df93916455
schandillia <amit@schandillia.com>
2025-01-01 20:15:08 +0530

    style: make navbar lighter than the rest of the page

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar = async () => {
   const user = await currentUser()
 
   return (
-    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 dark:border-gray-800 bg-white/80 dark:bg-brand-950/70 backdrop-blur-lg transition-all">
+    <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 dark:border-gray-800 bg-white/80 dark:bg-brand-900/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
         <div className="flex h-16 items-center justify-between">
           <Link href="/" className="flex z-40 font-semibold">
