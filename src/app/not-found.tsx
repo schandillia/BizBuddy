9e4838078c0a7eb9c3e2468571d377bb04d34f2a
schandillia <amit@schandillia.com>
2025-01-03 13:06:14 +0530

    style: change heading font in 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,15 @@
+import { Heading } from "@/components/heading"
+
 const Custom404 = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen dark:bg-black">
-      <h1 className="text-xl dark:text-gray-400">
+    <div className="flex items-center justify-center min-h-screen bg-white dark:bg-black">
+      <Heading as="h2" className="text-xl">
         <span>404</span>
-      </h1>
+      </Heading>
       <div className="border-l border-gray-300 mx-4 h-8"></div>
-      <h1 className="text-xl text-gray-600 dark:text-gray-300">
+      <p className="text-xl text-gray-600 dark:text-gray-300">
         <span>Page Not Found</span>
-      </h1>
+      </p>
     </div>
   )
 }
